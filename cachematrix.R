a3ef572e574c8413925ed608c0e64455e28ef9ab
ysha123 <yousof.shaladi@web.de>
2015-08-23 12:32:17 +0200

    Commiting the changes for ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  x <- NULL 
+  set <- function(y) {
+    x <<- y 
+    inv_x <<- NULL
+  }
+  get <- function() x
+  set_inv <- function(solve) inv_x <<- solve(x)
+  get_inv <- function() inv_x
+  list(set = set, get = get,
+       set_inv = set_inv, 
+       get_inv = get_inv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv_x <- x$get_inv()
+  if(!is.null(inv_x)) {
+    message("Retrieving value for inv_x")
+    return(inv_x)
+  }
+  data <- x$get
+  x <- solve(data, ...)
+  x$set_inv(inv_x)
+  inv_x
 }
